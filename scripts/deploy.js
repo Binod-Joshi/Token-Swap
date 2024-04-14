322b1b014e188899647164a2fcb88293c28699d8
Binod-Joshi <111906023+Binod-Joshi@users.noreply.github.com>
2024-04-15 02:13:02 +0530

    second commit on token swap

---
+++ scripts/deploy.js
@@ -0,0 +1,11 @@
+const factoryAddress = "0x10ED43C718714eb63d5aA57B78B54704E256024E";
+const routerAddress = "0x10ED43C718714eb63d5aA57B78B54704E256024E";
+const fromAddress = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56"; //BUSD
+const toAddress = "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c"; //WBNB
+
+module.exports = {
+    factoryAddress,
+    routerAddress,
+    fromAddress,
+    toAddress,
+}
